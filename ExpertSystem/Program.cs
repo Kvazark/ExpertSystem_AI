7aa94dc8790b653c31a82ce11037c000dd1ff33b
first <Katya_Klebleeva@mail.ru>
2022-05-30 20:05:44 +0300

    Update project

--- ExpertSystem/Program.cs
+++ ExpertSystem/Program.cs
@@ -22,83 +22,116 @@ rule.AddAntecedent(new IsClause("язык кандидата", "Ch"));
 rule.setConsequent(new IsClause("язык", "китайский"));
 inferenceEngine.AddRule(rule);
 
-rule = new Rule("Образование магистратура или бакалавриат?");
-rule.AddAntecedent(new IsClause("образование", "MD"));
-rule.setConsequent(new IsClause("высшее образование", "да"));
+rule = new Rule("Образование магистратура, бакалавриат или среднее специальное?");
+rule.AddAntecedent(new IsClause("уровень образования", "MD"));
+rule.setConsequent(new IsClause("образование", "магистратура"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Образование магистратура или бакалавриат?");
-rule.AddAntecedent(new IsClause("образование", "BD"));
-rule.setConsequent(new IsClause("высшее образование", "да"));
+rule.AddAntecedent(new IsClause("уровень образования", "BD"));
+rule.setConsequent(new IsClause("образование", "бакалавриат"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Образование магистратура или бакалавриат?");
-rule.AddAntecedent(new IsClause("образование", "GSE"));
-rule.setConsequent(new IsClause("высшее образование", "нет"));
+rule.AddAntecedent(new IsClause("уровень образования", "GSE"));
+rule.setConsequent(new IsClause("образование", "среднее специальное"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Опыт работы больше 3-х лет?");
-rule.AddAntecedent(new GreaterClause("опыт работы (в годах)", "0"));
-rule.AddAntecedent(new LessClause("опыт работы (в годах)", "2"));
+rule.AddAntecedent(new GreaterClause("опыт работы (в годах)", "-1"));
+rule.AddAntecedent(new LessClause("опыт работы (в годах)", "3"));
 rule.setConsequent(new IsClause("опыт работы", "нет"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Опыт работы больше 3-х лет?");
-rule.AddAntecedent(new GreaterClause("опыт работы (в годах)", "3"));
-rule.AddAntecedent(new LessClause("опыт работы (в годах)", "45"));
+rule.AddAntecedent(new GreaterClause("опыт работы (в годах)", "2.9"));
+rule.AddAntecedent(new LessClause("опыт работы (в годах)", "46"));
 rule.setConsequent(new IsClause("опыт работы", "да"));
 inferenceEngine.AddRule(rule);
 //////////////////////////////////////////////////////////\
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "английский"));
-rule.AddAntecedent(new IsClause("высшее образование", "да"));
+rule.AddAntecedent(new IsClause("образование", "магистратура"));
 rule.setConsequent(new IsClause("кандидат подходит", "да"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "английский"));
-rule.AddAntecedent(new IsClause("высшее образование", "нет"));
+rule.AddAntecedent(new IsClause("образование", "бакалавриат"));
+rule.setConsequent(new IsClause("кандидат подходит", "да"));
+inferenceEngine.AddRule(rule);
+
+rule = new Rule("Брать на работу?");
+rule.AddAntecedent(new IsClause("язык", "английский"));
+rule.AddAntecedent(new IsClause("образование", "среднее специальное"));
 rule.AddAntecedent(new IsClause("опыт работы", "да"));
 rule.setConsequent(new IsClause("кандидат подходит", "да"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "английский"));
-rule.AddAntecedent(new IsClause("высшее образование", "нет"));
+rule.AddAntecedent(new IsClause("образование", "среднее специальное"));
 rule.AddAntecedent(new IsClause("опыт работы", "нет"));
 rule.setConsequent(new IsClause("кандидат подходит", "нет"));
 inferenceEngine.AddRule(rule);
 ///////////////////////////
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "французский"));
-rule.AddAntecedent(new IsClause("высшее образование", "да"));
+rule.AddAntecedent(new IsClause("образование", "магистратура"));
+rule.setConsequent(new IsClause("кандидат подходит", "да"));
+inferenceEngine.AddRule(rule);
+
+rule = new Rule("Брать на работу?");
+rule.AddAntecedent(new IsClause("язык", "французский"));
+rule.AddAntecedent(new IsClause("образование", "бакалавриат"));
 rule.AddAntecedent(new IsClause("опыт работы", "да"));
 rule.setConsequent(new IsClause("кандидат подходит", "да"));
 inferenceEngine.AddRule(rule);
 
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "французский"));
-rule.AddAntecedent(new IsClause("высшее образование", "нет"));
+rule.AddAntecedent(new IsClause("образование", "бакалавриат"));
+rule.AddAntecedent(new IsClause("опыт работы", "нет"));
 rule.setConsequent(new IsClause("кандидат подходит", "нет"));
 inferenceEngine.AddRule(rule);
 
+rule = new Rule("Брать на работу?");
+rule.AddAntecedent(new IsClause("язык", "французский"));
+rule.AddAntecedent(new IsClause("образование", "среднее специальное"));
+rule.setConsequent(new IsClause("кандидат подходит", "нет"));
+inferenceEngine.AddRule(rule);
 
 rule = new Rule("Брать на работу?");
 rule.AddAntecedent(new IsClause("язык", "китайский"));
+rule.AddAntecedent(new IsClause("образование", "магистратура"));
+rule.AddAntecedent(new IsClause("опыт работы", "да"));
+rule.setConsequent(new IsClause("кандидат подходит", "да"));
+inferenceEngine.AddRule(rule);
+
+rule = new Rule("Брать на работу?");
+rule.AddAntecedent(new IsClause("язык", "китайский"));
+rule.AddAntecedent(new IsClause("образование", "бакалавриат"));
+rule.setConsequent(new IsClause("кандидат подходит", "нет"));
+inferenceEngine.AddRule(rule);
+
+rule = new Rule("Брать на работу?");
+rule.AddAntecedent(new IsClause("язык", "китайский"));
+rule.AddAntecedent(new IsClause("образование", "среднее специальное"));
 rule.setConsequent(new IsClause("кандидат подходит", "нет"));
 inferenceEngine.AddRule(rule);
 
 
 inferenceEngine.AddFact(new IsClause("язык кандидата", "Fr"));
-inferenceEngine.AddFact(new IsClause("образование", "MD"));
-inferenceEngine.AddFact(new IsClause("опыт работы (в годах)", "5"));
+inferenceEngine.AddFact(new IsClause("уровень образования", "BD"));
+inferenceEngine.AddFact(new IsClause("опыт работы (в годах)", "2"));
 
 inferenceEngine.Infer();
-Console.WriteLine("все факты:");
+Console.WriteLine("Все факты:");
 Console.WriteLine(inferenceEngine.Facts);
 
 Console.WriteLine(" ");
 var conclusion = inferenceEngine.Facts.IsFact(new IsClause("кандидат подходит", "да")); 
+conclusion = inferenceEngine.Facts.IsFact(new IsClause("кандидат подходит", "нет")); 
 Console.WriteLine("Вывод:");
-Console.WriteLine(conclusion ? "Кандидат подходит." : "Кандидат не подходит.");
+Console.WriteLine(conclusion ? "Кандидат определён." : "Логически кандидат не определён.");
 
